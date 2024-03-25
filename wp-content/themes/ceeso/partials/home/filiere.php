09cff608f673db5d58eb2af9416c57256b9f4983
pikloo <p.loukakou@gmail.com>
2024-03-25 18:05:40 +0100

    'Remplacement des images start et filière'

--- wp-content/themes/ceeso/partials/home/filiere.php
+++ wp-content/themes/ceeso/partials/home/filiere.php
@@ -1,5 +1,5 @@
-<section class="bg-secondary py-6 px-5 text-white my-14 xl:my-0  xl:py-20">
-    <figure class="grid grid-cols-1 gap-y-6 max-w-screen-xl mx-auto lg:gap-y-0 lg:grid-cols-2">
+<section class="bg-secondary py-6 px-5 text-white my-14 xl:my-0 xl:py-20">
+    <figure class="grid grid-cols-1 gap-y-6 max-w-screen-xl mx-auto lg:gap-y-0 lg:grid-cols-2 lg:gap-x-32 lg:items-center">
         <img class="w-fit mx-auto" src="./wp-content/themes/ceeso/assets/images/access_filiere_sante.png" alt="Accès filière de santé" />
         <figcaption>
             <span class="text-[15px] leading-[18px] tracking-[20%] font-light uppercase text-white">Filière</span>
